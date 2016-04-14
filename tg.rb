1c42b56f07ad4805dfa9a648e48a05f119610100
Dom Morgan <dom@d3r.com>
2016-04-14 22:03:14 +0100

    Update dependencies and use test branch of tg

--- tg.rb
+++ tg.rb
@@ -1,11 +1,11 @@
 class Tg < Formula
   desc "Marcello Mamino's Watch Timing Software"
   homepage "https://github.com/vacaboja/tg"
-  head 'https://github.com/vacaboja/tg.git', :branch => 'master'
+  head 'https://github.com/vacaboja/tg.git', :branch => 'test'
 
   depends_on "gtk+"
   depends_on "portaudio"
-  depends_on "fftw"
+  depends_on "fftw" => "with-openmp"
 
   def install
     system "make"
