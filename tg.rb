7015ea4c1b1f98a036bb350f499dc8ae2e4c6893
Dom Morgan <dom@d3r.com>
2016-10-18 20:59:44 +0100

    Give tg a version number

--- tg.rb
+++ tg.rb
@@ -1,7 +1,13 @@
 class Tg < Formula
   desc "Marcello Mamino's Watch Timing Software"
   homepage "https://github.com/vacaboja/tg"
-  head 'https://github.com/vacaboja/tg.git', :branch => 'master'
+  url "https://github.com/vacaboja/tg.git", :revision => "d4acda0aa94c112228731d4ac1d51fa1d4e1944e"
+  version "0.3.2"
+  head "https://github.com/vacaboja/tg.git", :branch => "master"
+
+  # devel do
+  #   url "https://github.com/vacaboja/tg.git", :branch => "test"
+  # end
 
   depends_on "gtk+"
   depends_on "portaudio"
