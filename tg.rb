1b819f5c484cf2b801b9ceb2a20c1dbf4b5ffa58
Dom Morgan <dom@d3r.com>
2017-06-13 00:41:35 +0100

    Get ready for new version

--- tg.rb
+++ tg.rb
@@ -1,20 +1,23 @@
 class Tg < Formula
   desc "Marcello Mamino's Watch Timing Software"
-  homepage "https://github.com/vacaboja/tg"
+  homepage "https://tg.ciovil.li"
   url "https://github.com/vacaboja/tg.git", :tag => "v0.4.0"
   version "0.4.0"
   head "https://github.com/vacaboja/tg.git", :branch => "master"
 
-  depends_on "gtk+"
+  devel do
+    url "https://github.com/vacaboja/tg.git", :branch => "test"
+    version "0.5.0"
+  end
+
+  depends_on "gtk+3"
   depends_on "portaudio"
   depends_on "fftw"
 
   def install
     system "make"
     bin.install "build/tg"
-    bin.install "build/tg-lt"
     mv "#{bin}/tg", "#{bin}/tg-timer"
-    mv "#{bin}/tg-lt", "#{bin}/tg-timer-lt"
     man1.install "docs/tg-timer.1.gz"
   end
 
