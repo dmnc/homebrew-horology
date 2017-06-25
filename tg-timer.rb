458860c0a354eca148a30681e6a5778e5eb2b14a
Dom Morgan <dom@d3r.com>
2017-06-25 23:32:56 +0100

    Update description

--- tg-timer.rb
+++ tg-timer.rb
@@ -1,5 +1,5 @@
 class TgTimer < Formula
-  desc "A program for timing mechanical watches"
+  desc "Tool for timing mechanical watches"
   homepage "https://tg.ciovil.li"
   url "https://github.com/vacaboja/tg/archive/v0.5.0.tar.gz"
   sha256 "c88889bd391212d9104bca9918fb573a4ff96a752717b4a779d33f637c633ce7"
