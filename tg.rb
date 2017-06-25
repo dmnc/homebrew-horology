4f97be752029d6dd2aa5d687be8524861f9cd107
Dom Morgan <dom@d3r.com>
2017-06-25 22:03:47 +0100

    Update build mechanism and bump version

--- tg.rb
+++ tg.rb
@@ -1,21 +1,19 @@
 class Tg < Formula
   desc "Marcello Mamino's Watch Timing Software"
   homepage "https://tg.ciovil.li"
-  url "https://github.com/vacaboja/tg.git", :tag => "v0.4.0"
-  version "0.4.0"
+  url "https://github.com/vacaboja/tg.git", :tag => "v0.5.0"
+  version "0.5.0"
   head "https://github.com/vacaboja/tg.git", :branch => "master"
 
-  devel do
-    url "https://github.com/vacaboja/tg.git", :branch => "test"
-    version "0.5.0"
-  end
-
+  depends_on "automake" => :run
   depends_on "gtk+3"
   depends_on "portaudio"
   depends_on "fftw"
   depends_on "gnome-icon-theme"
 
   def install
+    system "./autogen.sh"
+    system "./configure"
     system "make"
     bin.install "build/tg"
     mv "#{bin}/tg", "#{bin}/tg-timer"
