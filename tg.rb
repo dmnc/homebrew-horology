9b002f367ed9549f01950eacecda393883743297
Dom Morgan <dom@d3r.com>
2017-06-25 22:47:37 +0100

    Account for new build locations

--- tg.rb
+++ tg.rb
@@ -2,22 +2,23 @@ class Tg < Formula
   desc "Marcello Mamino's Watch Timing Software"
   homepage "https://tg.ciovil.li"
   url "https://github.com/vacaboja/tg.git", :tag => "v0.5.0"
-  version "0.5.0"
   head "https://github.com/vacaboja/tg.git", :branch => "master"
 
   depends_on "automake" => :run
+  depends_on "libtool" => :run
   depends_on "gtk+3"
   depends_on "portaudio"
   depends_on "fftw"
   depends_on "gnome-icon-theme"
 
   def install
+    system "export LIBTOOL=`which glibtool`"
+    system "export LIBTOOLIZE=`which glibtoolize`"
     system "./autogen.sh"
     system "./configure"
     system "make"
-    bin.install "build/tg"
-    mv "#{bin}/tg", "#{bin}/tg-timer"
-    man1.install "docs/tg-timer.1.gz"
+    bin.install "tg-timer"
+    man1.install "docs/tg-timer.1"
   end
 
   test do
